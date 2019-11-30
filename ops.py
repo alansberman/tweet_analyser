226b5bfa799fa67cc50285976a6241f9f78ac629
Alan Berman <alan.berman.92@gmail.com>
2019-11-30 15:19:58 +0200

    Sentiment analysis works! Tweets posted into the form are classified as positive or negative. MySQL still TODO

--- ops.py
+++ ops.py
@@ -1,6 +1,9 @@
 # Classifies tweets
-
+from flask_wtf import FlaskForm 
+from wtforms import TextField, SubmitField
+from wtforms.validators import DataRequired
 import pickle
+from sklearn.naive_bayes import MultinomialNB
 
 # load the stored sklearn classifier
 def load_classifier(path='assets/tweet_classifier_model.sav'):
@@ -9,5 +12,11 @@ def load_classifier(path='assets/tweet_classifier_model.sav'):
 # classify tweet
 # 0 = negative tweet, 1 = positive tweet
 def classify_tweet(classifier,tweet):
-    prediction = classifier.predict(list(tweet))[0]
+    prediction = classifier.predict(tweet)[0]
     return prediction
+
+# Form to submit tweets for classification
+# Thanks to https://hackersandslackers.com/forms-in-flask-wtforms/
+class TweetForm(FlaskForm):
+    tweet = TextField('Tweet',[DataRequired()])
+    submit = SubmitField('Submit Tweet')
--- server.py
+++ server.py
@@ -3,14 +3,28 @@
 # Author: Alan Berman
 
 from flask import Flask, request, redirect, render_template
-
+from flask import current_app
+from ops import *
 app = Flask(__name__)
+app.config['SECRET_KEY']="yabbadabbado"
+# load classifier model on start 
+app.classifier = load_classifier()
 
-@app.route("/",methods=["POST","GET"])
+@app.route("/",methods=("POST","GET"))
 def home():
-    # if submitting a tweet        
-    return render_template("home.html")
+    # if submitting a tweet
+    form = TweetForm()
+    if form.validate_on_submit():
+        return redirect("/result.html")
+    return render_template("home.html",form=form)
 
+# Thanks to https://stackoverflow.com/questions/55117243/field-validation-in-wtf-forms-flask-redirect-method
+@app.route("/result",methods=("POST","GET"))
+def result():
+    # Classify tweet, return result
+    tweet = request.form['tweet']
+    result = classify_tweet(current_app.classifier,[tweet])
+    return render_template("/result.html",tweet=tweet, result=result)
 
 if "__name__"=='__main__':
     app.run()
